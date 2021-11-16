4f337c54c34bfe7c0c33a8997b1d8f5f6f3588a6
Tomas Daugela <todaprojects@gmail.com>
2021-11-16 22:17:56 +0200

    Installs and configures 'AutoMapper'

---
+++ src/MyAccounting.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyAccounting.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
--- src/MyAccounting.WebApi/Startup.cs
+++ src/MyAccounting.WebApi/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using MyAccounting.Application;
 using MyAccounting.Infrastructure;
 
 namespace MyAccounting.WebApi
@@ -20,6 +21,7 @@ namespace MyAccounting.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplication();
             services.AddInfrastructure(Configuration);
             
             services.AddControllers();
